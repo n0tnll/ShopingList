7291f17dddd43d7624ad50bf5b6734e4cc1ec1ce
Sergey <frostfreerun@gmail.com>
2022-12-24 13:51:15 +0300

    Updated ShopListRepositoryImpl.kt with Transformations.map

--- app/src/main/java/com/shv/android/shopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/shv/android/shopinglist/data/ShopListRepositoryImpl.kt
@@ -2,10 +2,9 @@ package com.shv.android.shopinglist.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.shv.android.shopinglist.domain.ShopItem
 import com.shv.android.shopinglist.domain.ShopListRepository
-import kotlin.random.Random
 
 class ShopListRepositoryImpl(
     application: Application
@@ -14,8 +13,10 @@ class ShopListRepositoryImpl(
     private val shopListDao = AppDatabase.getInstance(application).shopListDao()
     private val mapper = ShopListMapper()
 
-    override fun getShopItemList(): LiveData<List<ShopItem>> {
-        return shopListDao.getShopList()
+    override fun getShopItemList(): LiveData<List<ShopItem>> = Transformations.map(
+        shopListDao.getShopList()
+    ) {
+        mapper.mapListDbModelToListEntity(it)
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
